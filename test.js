c3bcf83f42729b87401ac0ca0959d5f402b5e18c
Michael Rome <michael@ncie.idx.org.au>
2015-07-03 15:59:58 +1000

    added a block

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 (function(ext) {
-	// Cleanup function when the extension is unloaded
-	ext._shutdown = function() {};
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
-	// Status reporting code
-	// Use this to report missing hardware, plugin or unsupported browser
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
-	// Block and block menu descriptions
-	var descriptor = {
-		blocks: [
-		]
-	};
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
+    };
 
-	// Register the extension
-	ScratchExtensions.register('Sample extension', descriptor, ext);
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
